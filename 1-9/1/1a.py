b14aaf9018a5fce4ad85a66382b17f70dfb50afd
danielletrinh <56095472+danielletrinh@users.noreply.github.com>
2021-11-30 21:28:13 -0800

    day 1 minor consolidations

--- 1-9/1/1a.py
+++ 1-9/1/1a.py
@@ -5,9 +5,7 @@ data_list = data.readlines()
 total_increased = 0
 
 for i in range(len(data_list) - 1):
-    n1, n2 = int(data_list[i]), int(data_list[i+1])
-    increased = (n2 > n1)
-    if (increased):
+    if (int(data_list[i+1]) > int(data_list[i])):
         total_increased += 1
 
 print(total_increased)
--- 1-9/1/1b.py
+++ 1-9/1/1b.py
@@ -6,10 +6,8 @@ prev_sum = 0
 total_increased = -1
 
 for i in range(len(data_list) - 2):
-    n1, n2, n3 = int(data_list[i]), int(data_list[i+1]), int(data_list[i+2])
-    increased = (n1 + n2 + n3) > prev_sum
-    if (increased):
+    if (int(data_list[i]) + int(data_list[i+1]) + int(data_list[i+2]) > prev_sum):
         total_increased += 1
-    prev_sum = n1 + n2 + n3
+    prev_sum = int(data_list[i]) + int(data_list[i+1]) + int(data_list[i+2])
 
 print(total_increased)
